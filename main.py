d98f5ceb9e0bec222e63196d9e404d860314b7b8
Nayan Gupta <ngupta@csumb.edu>
2018-08-07 23:27:27 +0000

    Fixed third HTML

--- main.py
+++ main.py
@@ -19,29 +19,22 @@ class AboutPage(webapp2.RequestHandler):
     def get(self):
         about_template = the_jinja_env.get_template('templates/about.html')
         self.response.write(about_template.render())
-   
-
 
 class InfoPage(webapp2.RequestHandler):
     def get(self):
         about_template = the_jinja_env.get_template('templates/info.html')
         self.response.write(about_template.render())
-    def post(self):
-        self.response.write("Received a post request")
-        
-class MainInterface(webapp2.RequestHandler):
-    def get(self):
-        about_template = the_jinja_env.get_template('templates/main_interface.html')
-        self.response.write(about_template.render())
-    def post(self):
-        self.response.write("Received a post request")
 
+class Interface(webapp2.RequestHandler):
+    def post(self):
+        about_template = the_jinja_env.get_template('templates/interface.html')
+        self.response.write(about_template.render())
 
 
 app = webapp2.WSGIApplication([
     ('/', AboutPage),
     ('/info', InfoPage),
-    ('/main', MainInterface),
+    ('/interface', Interface),
 ], debug=True)
 
 
