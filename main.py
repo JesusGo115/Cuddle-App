5c42b1e66b2989ea3931ec70d27e4603aefa82fb
Marlene Alonzo <maalonzo@csumb.edu>
2018-08-08 16:50:52 +0000

    merge conflict

--- main.py
+++ main.py
@@ -48,14 +48,12 @@ class MainInterface(webapp2.RequestHandler):
         self.response.write(about_template.render())
     def post(self):
         self.response.write("Received a post request")
-<<<<<<< HEAD
         
         
 
 app = webapp2.WSGIApplication([
     ('/', AboutPage),
     ('/info', InfoPage),
-=======
 
 class InterfacePage(webapp2.RequestHandler):
     def post(self):
@@ -67,7 +65,6 @@ app = webapp2.WSGIApplication([
     ('/', StartPage),
     ('/signin', SignInPage),
     ('/signup', SignUpPage),
->>>>>>> e71c389e78a6bfd28d80881a5c340360c1cff274
     ('/interface', InterfacePage),
 ], debug=True)
 
