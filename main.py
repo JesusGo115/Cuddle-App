89ab92736cfa8cce481d9b5541be1a25a2dc5399
Anita Garcia <anitagarcia819@gmail.com>
2018-08-08 18:50:36 +0000

    introduced some bugs

--- main.py
+++ main.py
@@ -15,27 +15,30 @@ the_jinja_env = jinja2.Environment(
 
 class AboutPage(webapp2.RequestHandler):
     def get(self):
+        print "=== AboutPage (get) ==="
         about_template = the_jinja_env.get_template('templates/about.html')
         self.response.write(about_template.render())
-   
+        
+        print "Welcome to day " + 13 + " of CSSIx!"
+        
 class InputPage(webapp2.RequestHandler):
     def get(self):
+        print "=== InputPage (get) ==="
         input_template = the_jinja_env.get_template('templates/input.html')
         self.response.write(input_template.render())
-
+ 
     def post(self):
-        isError = False
-        if isError:
-            self.response.write("Error! : ( ")
-        else:
-            self.redirect("/")
-
+        print "=== InputPage (post) ==="
+        self.redirect("/")
+   
 class EchoPage(webapp2.RequestHandler):
     def get(self):
+        print "=== EchoPage (get) ==="
         input_template = the_jinja_env.get_template('templates/echo.html')
         self.response.write(input_template.render())
         
     def post(self):
+        print "=== EchoPage (post) ==="
         results_template = the_jinja_env.get_template('templates/echo.html')
         user_input = self.request.get('user-input')
         
