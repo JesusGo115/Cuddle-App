e40a5826562e011e224fc224eab0cbc2ff368fc9
Nayan Gupta <ngupta@csumb.edu>
2018-08-08 00:07:13 +0000

    safe

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import os
 from google.appengine.api import urlfetch
 import json
 
-
 the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
@@ -24,10 +23,19 @@ class AboutPage(webapp2.RequestHandler):
 
 class InfoPage(webapp2.RequestHandler):
     def get(self):
-        about_template = the_jinja_env.get_template('templates/info.html')
-        self.response.write(about_template.render())
+        self.session = Session()
+        if "email" in self.session: 
+            self.response.write ("Hello " + self.session["email"])
+        else:    
+            about_template = the_jinja_env.get_template('templates/info.html')
+            self.response.write(about_template.render())
+    
     def post(self):
         self.response.write("Received a post request")
+        email = self.request.get("email")
+        self.response.write(email)
+        self.session = Session()
+        self.session['email'] = email
         
 class MainInterface(webapp2.RequestHandler):
     def get(self):
@@ -35,7 +43,9 @@ class MainInterface(webapp2.RequestHandler):
         self.response.write(about_template.render())
     def post(self):
         self.response.write("Received a post request")
-
+        
+        
+        
 
 
 app = webapp2.WSGIApplication([
