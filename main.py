515f12ded410234a041f0a9efc719945cf676c77
Jesus Gomez <gmjesus95@gmail.com>
2018-08-09 18:29:08 +0000

    uhgytbyg

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ the_jinja_env = jinja2.Environment(
     autoescape=True)
 
 def get_all_people(self):
+    print "=====(get_all_people)===="
     all_people = Peoples.query().fetch()
         
     the_variable_dict = {
@@ -22,6 +23,7 @@ def get_all_people(self):
 def run_query(first_line, second_line, third_line, fourth_line, fifth_line):
     people = Peoples(name=first_line, age = second_line, gender = third_line, number = fourth_line, zip_code = fifth_line)
     people_key = people.put()
+    #print "====(run_query)====="print people_key
 
     
     
@@ -40,39 +42,27 @@ class SignInPage(webapp2.RequestHandler):
 class SignUpPage(webapp2.RequestHandler):
     def get(self):
         about_template = the_jinja_env.get_template('templates/signup.html')
-        '''
-        people_first_line = self.request.get('name')
-        people_second_line = self.request.get('age')
-        people_third_line = self.request.get('gender')
-        people_fourth_line = self.request.get('number')
-        people_fifth_line = self.request.get('zip')
-        
-        run_query(people_first_line, people_second_line, people_third_line, people_fourth_line, people_fifth_line)
-        
-        the_variable_dict = {"line1": people_first_line,
-                             "line2": people_second_line,
-                             "line3": people_third_line,
-                             "line4": people_fourth_line,
-                             "line5": people_fifth_line
-                             }
-                             '''
         self.response.write(about_template.render())
 
 class InterfacePage(webapp2.RequestHandler):
     def post(self):
+        print "====InterfacePage (post)====="
         interface_template = the_jinja_env.get_template('templates/interface.html')
         people_name = self.request.get('name')
         people_age = self.request.get('age')
         people_gender = self.request.get('gender')
         people_number = self.request.get('number')
         people_zip_code = self.request.get('zip')
+        print(people_name, people_age)
+        run_query(people_name, people_age, people_gender, people_number, people_zip_code)
         
-        run_query(people_age, people_age, people_gender, people_number, people_zip_code)
-        
-        the_variable_dict = get_all_people(self)
+        #the_variable_dict = get_all_people(self)
     
-        self.response.write(interface_template.render(the_variable_dict))
+        #self.response.write(interface_template.render(the_variable_dict))
+        self.redirect('/interface')
     def get(self):
+        print "====InterfacePage (get)====="
+
         interface_template = the_jinja_env.get_template('templates/interface.html')
         
         the_variable_dict = get_all_people(self)
